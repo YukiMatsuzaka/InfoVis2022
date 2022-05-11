2021abd9ecd52242669039517b4c8c8375b14a1f
松坂　勇樹 <matsuzakayuki@matsuzakayuukinoMacBook-Pro-2.local>
2022-05-11 21:31:28 +0900

    W08_task

--- W08/w08_task2_main.js
+++ W08/w08_task2_main.js
@@ -1,29 +1,29 @@
-d3.csv("https://yukimatsuzaka.github.io/InfoVis2022/W04/w04_task2.csv")
+d3.csv("https://yukimatsuzaka.github.io/InfoVis2022/W08/w08_task2.csv")
     .then( data => {
-        data.forEach( d => { d.label = d.label; d.width = +d.width; d.color = d.color});
+        data.forEach( d => { d.x = +d.x; d.y = +d.y; d.r=+d.r;});
 
         var config = {
             parent: '#drawing_region',
-            width: 256,
+            width: 500,
             height: 128,
-            margin: {top:10, right:30, bottom:20, left:60}
+            margin: {top:10, right:20, bottom:20, left:30}
         };
 
-        const barchart = new BarChart( config, data );
-        barchart.update();
+        const linechart = new LineChart( config, data );
+        linechart.update();
     })
     .catch( error => {
         console.log( error );
     });
 
-class BarChart {
+class LineChart {
 
     constructor( config, data ) {
         this.config = {
             parent: config.parent,
             width: config.width || 256,
             height: config.height || 256,
-            margin: config.margin || {top:10, right:10, bottom:10, left:10}
+            margin: config.margin || {top:10, right:40, bottom:10, left:10}
         }
         this.data = data
         this.init();
@@ -43,27 +43,29 @@ class BarChart {
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
         self.xscale = d3.scaleLinear()
-            .domain([0, d3.max(self.data, d => d.width )])
+            .domain([0, d3.max(self.data, d => d.x )])
             .range( [0, self.inner_width] );
 
-        self.yscale = d3.scaleBand()
-            .domain(self.data.map(d => d.label))
+        self.yscale = d3.scaleLinear()
+            .domain([0, d3.max(self.data, d => d.y )])
             .range( [0, self.inner_height] )
-            .paddingInner(0.1);
 
         self.xaxis = d3.axisBottom( self.xscale )
             .ticks(10)
             .tickSizeOuter(0);
-        
-        
+          
         self.yaxis = d3.axisLeft( self.yscale)
+            .ticks(5)
             .tickSizeOuter(0);
         
         self.xaxis_group = self.chart.append('g')
             .attr('transform', `translate(0, ${self.inner_height})`);
         
-        
-        self.yaxis_group = self.chart.append('g');
+        self.yaxis_group = self.chart.append('g')
+
+        self.line = d3.line()
+            .x( d => self.xscale(d.x))
+            .y( d => self.yscale(d.y));
         
     }
 
@@ -76,23 +78,27 @@ class BarChart {
     render() {
         let self = this;
 
-
         self.xaxis_group
             .call(self.xaxis);
         
         self.yaxis_group
             .call(self.yaxis);
 
-        self.chart.selectAll("rect")
+        self.chart.append('path')
+            .data(self.data)
+            .attr('d', self.line(self.data))
+            .attr('stroke', 'black')
+            .attr('fill', 'none')
+
+
+        
+        self.chart.selectAll("circle")
             .data(self.data)
             .enter()
-            .append("rect")
-            .attr("x", 0)
-            .attr("y", d => self.yscale(d.label))
-            .attr("width", d => self.xscale(d.width))
-            .attr("height", self.yscale.bandwidth())
-            .style("fill", function(d){ return d.color; });
-        
+            .append("circle")
+            .attr("cx", d => self.xscale( d.x ) )
+            .attr("cy", d => self.yscale( d.y ) )
+            .attr("r", d => d.r);
 
     }
 }
