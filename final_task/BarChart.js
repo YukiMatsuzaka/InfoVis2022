28a093eff03f362db212e8de102d6af93b1b5798
松坂　勇樹 <matsuzakayuki@matsuzakayuukinoMacBook-Pro-2.local>
2022-06-11 17:41:02 +0900

    final_task

--- final_task/BarChart.js
+++ final_task/BarChart.js
@@ -99,31 +99,59 @@ class BarChart {
             self.yscale.domain( [0,ymax] );
         }
         else if (key == 'Food_Beverages') {
-            const ymax = d3.max( self.data, d => d.Food_Beverages );
+            let ymax = d3.max( self.data, d => d.Food_Beverages );
+            let ymax_2019 = d3.max( self.data, d => d.Food_Beverages_2019 );
+            if ( ymax < ymax_2019){
+                ymax = ymax_2019;
+            }
             self.yscale.domain( [0,ymax] );
         }
         else if (key == 'Building_Materials') {
-            const ymax = d3.max( self.data, d => d.Building_Materials );
+            let ymax = d3.max( self.data, d => d.Building_Materials );
+            let ymax_2019 = d3.max( self.data, d => d.Building_Materials_2019 );
+            if ( ymax < ymax_2019){
+                ymax = ymax_2019;
+            }
             self.yscale.domain( [0,ymax] );
         }
         else if (key == 'Chemicals') {
-            const ymax = d3.max( self.data, d => d.Chemicals );
+            let ymax = d3.max( self.data, d => d.Chemicals );
+            let ymax_2019 = d3.max( self.data, d => d.Chemicals_2019 );
+            if ( ymax < ymax_2019){
+                ymax = ymax_2019;
+            }
             self.yscale.domain( [0,ymax] );
         }
         else if (key == 'Minerals_Metals') {
-            const ymax = d3.max( self.data, d => d.Minerals_Metals );
+            let ymax = d3.max( self.data, d => d.Minerals_Metals );
+            let ymax_2019 = d3.max( self.data, d => d.Minerals_Metals_2019 );
+            if ( ymax < ymax_2019){
+                ymax = ymax_2019;
+            }
             self.yscale.domain( [0,ymax] );
         }
         else if (key == 'Industry_Machinery_Equipment') {
-            const ymax = d3.max( self.data, d => d.Industry_Machinery_Equipment );
+            let ymax = d3.max( self.data, d => d.Industry_Machinery_Equipment );
+            let ymax_2019 = d3.max( self.data, d => d.Industry_Machinery_Equipment_2019 );
+            if ( ymax < ymax_2019){
+                ymax = ymax_2019;
+            }
             self.yscale.domain( [0,ymax] );
         }
         else if (key == 'Motor_Vehicles') {
-            const ymax = d3.max( self.data, d => d.Motor_Vehicles );
+            let ymax = d3.max( self.data, d => d.Motor_Vehicles );
+            let ymax_2019 = d3.max( self.data, d => d.Motor_Vehicles_2019 );
+            if ( ymax < ymax_2019){
+                ymax = ymax_2019;
+            }
             self.yscale.domain( [0,ymax] );
         }
         else if (key == 'Electorical_Machinery_Equipment') {
-            const ymax = d3.max( self.data, d => d.Electorical_Machinery_Equipment );
+            let ymax = d3.max( self.data, d => d.Electorical_Machinery_Equipment );
+            let ymax_2019 = d3.max( self.data, d => d.Electorical_Machinery_Equipment_2019 );
+            if ( ymax < ymax_2019){
+                ymax = ymax_2019;
+            }
             self.yscale.domain( [0,ymax] );
         }
 
@@ -194,42 +222,70 @@ class BarChart {
                 .attr("y", d => self.yscale(d.Food_Beverages))
                 .attr("height", d => self.inner_height - self.yscale(d.Food_Beverages))
                 .attr("width", self.xscale.bandwidth())
+
+            this.circles
+                .attr("cy", d => self.yscale( d.Food_Beverages_2019 ) )
+                .attr("fill", "blue" )
         }
         else if (key == 'Building_Materials') {
             this.rect
                 .attr("y", d => self.yscale(d.Building_Materials))
                 .attr("height", d => self.inner_height - self.yscale(d.Building_Materials))
                 .attr("width", self.xscale.bandwidth())
+
+            this.circles
+                .attr("cy", d => self.yscale( d.Building_Materials_2019 ) )
+                .attr("fill", "blue" )
         }
         else if (key == 'Chemicals') {
             this.rect
                 .attr("y", d => self.yscale(d.Chemicals))
                 .attr("height", d => self.inner_height - self.yscale(d.Chemicals))
                 .attr("width", self.xscale.bandwidth())
+
+            this.circles
+                .attr("cy", d => self.yscale( d.Chemicals_2019 ) )
+                .attr("fill", "blue" )
         }
         else if (key == 'Minerals_Metals') {
             this.rect
                 .attr("y", d => self.yscale(d.Minerals_Metals))
                 .attr("height", d => self.inner_height - self.yscale(d.Minerals_Metals))
                 .attr("width", self.xscale.bandwidth())
+
+            this.circles
+                .attr("cy", d => self.yscale( d.Minerals_Metals_2019 ) )
+                .attr("fill", "blue" )
         }
         else if (key == 'Industry_Machinery_Equipment') {
             this.rect
                 .attr("y", d => self.yscale(d.Industry_Machinery_Equipment))
                 .attr("height", d => self.inner_height - self.yscale(d.Industry_Machinery_Equipment))
                 .attr("width", self.xscale.bandwidth())
+
+            this.circles
+                .attr("cy", d => self.yscale( d.Industry_Machinery_Equipment_2019 ) )
+                .attr("fill", "blue" )
         }
         else if (key == 'Motor_Vehicles') {
             this.rect
                 .attr("y", d => self.yscale(d.Motor_Vehicles))
                 .attr("height", d => self.inner_height - self.yscale(d.Motor_Vehicles))
                 .attr("width", self.xscale.bandwidth())
+
+            this.circles
+                .attr("cy", d => self.yscale( d.Motor_Vehicles_2019 ) )
+                .attr("fill", "blue" )
         }
         else if (key == 'Electorical_Machinery_Equipment') {
             this.rect
                 .attr("y", d => self.yscale(d.Electorical_Machinery_Equipment))
                 .attr("height", d => self.inner_height - self.yscale(d.Electorical_Machinery_Equipment))
                 .attr("width", self.xscale.bandwidth())
+
+            this.circles
+                .attr("cy", d => self.yscale( d.Electorical_Machinery_Equipment_2019 ) )
+                .attr("fill", "blue" )
         }
 
         self.xaxis_group
